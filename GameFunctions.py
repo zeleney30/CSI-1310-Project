33b97e0cbe8e8aa5ca4e4bf0b79e1c1c50db2fbd
zeleney30 <michaelzeleney@gmail.com>
2022-03-30 15:17:11 -0400

    Fixed Skill Points
    Also separated some functions into their own files to easier understand code

---
+++ GameFunctions.py
@@ -0,0 +1,85 @@
+from os import system, name
+
+import SkillPoints as Skills
+
+#Only works in terminal, not IDLE shell
+def ClearTerminal():
+    if name == "nt":
+        _ = system("cls")
+    else:
+        _ = system("clear")
+
+menu={'A': '- Continue Game', 'B': '- New Game', 'C':'- Save Game', 'D':'- Quit Game'} #open automatically on game startup
+
+while True:
+    options = menu.keys()
+    for entry in options:
+        print(entry, menu[entry])
+        f = open("PythonGame.txt", 'w+')
+        f.close()
+        #write save data
+        
+
+    selection=input("Please select one of the following options: ")
+    if selection == 'A' or selection == 'a':
+        #load previous game
+        print("Loading game...")
+        break
+    elif selection == 'B' or selection == 'b':
+        #start new game
+        f = open("Pythongame.txt", 'w+')
+        f.truncate()
+        f.close()
+        print("Starting new game...")
+        break
+    elif selection == 'C' or selection == 'c':
+        #save the game
+        print("Saving game...")
+        break
+    elif selection == 'D' or selection == 'd':
+        #close the game -- autosave before closing
+        print("Closing game...")
+        break
+        quit()
+        
+    else:
+        print("Please select a valid option.")
+        #open the menu again so they can choose a valid option
+
+def PrintMenu():
+    if selection == 'A' or selection == 'a' or selection == 'B' or selection == 'b':
+        print("Enter 'S' to view your skill points")
+        print("Enter 'T' to talk to the trader")
+        print("Enter 'D' to enter the dungeon")
+        print("Enter 'X' to close the menu.")
+        print("Enter 'Z' to close the game.")
+
+
+    key = input("")
+    
+    if key == "S" or key == "s":
+        Skills.PrintSkills()
+        
+
+        if key == "E" or key == "e":
+            PrintMenu()
+                
+    if key == "D" or key == "d":
+        print("Dungeon")
+        #enter dungeon
+    if key == "T" or key == "t":
+        #enter trader
+        #load trader inventory from file
+        #after x time, refresh with new items -- 15 minutes? 5 minutes?
+        GenerateTraderInv()
+        for i in traderInventory:
+            print(i)
+
+    if key == 'X' or key == 'x':
+        #close the PrintMenu
+        print("Closing menu...")
+
+    if key == 'Z' or key == 'z':
+        #close the game
+        print("Closing game...")
+        quit()
---
+++ SkillPoints.py
@@ -0,0 +1,115 @@
+from os import system, name
+from os.path import exists
+import os
+
+import GameFunctions as Game
+
+
+sp = 5
+skills = [["1 - Strength: ", 0], ["2 - Dexterity: ", 0], ["3 - Intelligence: ", 0], ["4 - Agility: ", 0], ["5 - Stamina: ", 0]]
+
+def getSP():
+    return sp
+
+#Create the save file if it doesnt already exist
+def CreateSaveFile():
+    if not exists("Test.txt"):
+        file = open("Test.txt", "x")
+        file.close()
+CreateSaveFile()
+
+#Save skills
+def SaveSkills():
+    file = open("Test.txt", "w")
+    for i in skills:
+        for k in i:
+            file.write(str(k) + "\n")
+
+    file.write(str(sp) + "\n")
+    file.close()
+
+#Load skills
+def LoadSkills():
+    if not os.path.getsize("Test.txt") > 0:
+        return
+    
+    global skills
+    global sp
+    
+    file = open("Test.txt", "r")
+    
+    list1 = []
+    list2 = []
+
+    i = 0
+
+    while True:
+        i += 1
+
+        line = file.readline()
+
+        if not line:
+            break
+
+        if not (i == 11):
+            if (i % 2) == 1:
+                list1.insert(i, line.strip() + " ")
+            else:
+                list2.insert(i, int(line.strip()))
+        else:
+            sp = int(line.strip())
+    
+    file.close()
+
+    list3 = []
+
+    j = 0
+
+    while j < len(list1):
+        list3.insert(j, [list1[j], list2[j]])
+        j += 1
+
+    skills = list3     
+
+#Print skills
+def PrintSkills():
+    Game.ClearTerminal()
+    for i in skills:
+        for k in i:
+            print(k, end = "")
+        print()
+    print("Available skill points: ", sp)
+    
+    if sp > 0:
+        print("Enter 'U' to spend skill points")
+        
+    key = input("")
+    
+    if key == "U" or key == "u":
+        UpgradeSkills()
+    elif key == "E" or key == "e":
+        Game.PrintMenu()
+
+#Upgrade skills
+def UpgradeSkills():
+    global sp
+    
+    while sp > 0:
+        if sp <= 0:
+            PrintSkills()
+            break
+            
+        upgrade = input("Skill to upgrade: ")
+        
+        if upgrade == "E" or upgrade == 'e':
+            PrintSkills()
+            break
+        else:
+            upgrade = int(upgrade)
+            upgrade -= 1
+            if (upgrade >= 0 & upgrade <= 5):
+                skills[upgrade][1] += 1
+                sp -= 1
+                SaveSkills()
+                PrintSkills()
+    
