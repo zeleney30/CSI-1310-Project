561a903dfaeb91ba22364431ea5008f674b90939
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2022-04-18 16:28:00 -0400

    removed line from groupproject.py

--- GroupProject.py
+++ GroupProject.py
@@ -1,17 +1,7 @@
 import GameFunctions as Game
 import Player as Player
 import SkillPoints as Skills
-from breezypythongui import EasyFrame
 
-class popupWindow(EasyFrame):
-    """Displays a greeting in a window"""
-    def __init__(self):
-        EasyFrame.__init__(self)
-        self.addLabel(text = "Welcome to Mini Dungeon Simulator", row = 0, column = 0)
-def main():
-    popupWindow().mainloop()
-
-main()
 
 
 Skills.Load()
